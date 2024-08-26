08cfb7eb32c960b43a52a0caa044c2d851ea3026
rudrapatel001 <rudra7042004@gmail.com>
2024-08-27 01:02:41 +0530

    scroll to top button

--- script.js
+++ script.js
@@ -100,4 +100,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+    const scrollTopBtn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
